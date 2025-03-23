a12593cfb1e33606e2a24ced398be0cf336b9804
yunchan312 <phenomenal312@hufs.ac.kr>
2025-03-24 00:27:44 +0900

    no data

--- src/Components/NoData.tsx
+++ src/Components/NoData.tsx
@@ -4,11 +4,18 @@ import Warning from "../assets/Warning.svg";
 const NoData = () => {
   const navigate = useNavigate();
   return (
-    <div className="flex flex-col items-center justify-center gap-2 h-[50vh]">
-      <img src={Warning} alt="warning" className="size-[100px] mt-10" />
-      <div>해당하는 청원이 없습니다.</div>
-      <div className="hufsBtn rounded-md" onClick={() => navigate("/petition")}>
-        내가 청원하러 가기
+    <div className="h-[50vh]">
+      <div className="flex flex-col items-center justify-center gap-2 h-full">
+        <img src={Warning} alt="warning" className="size-[100px] mt-10" />
+        <div>해당하는 청원이 없습니다.</div>
+        <div className="w-[300px]">
+          <div
+            className="hufsBtn rounded-md"
+            onClick={() => navigate("/petition")}
+          >
+            내가 청원하러 가기
+          </div>
+        </div>
       </div>
     </div>
   );
